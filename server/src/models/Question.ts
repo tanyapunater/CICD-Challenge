0579864e256772db1936b87e67ebb4b93094f922
tanya <tanya.punater@yahoo.com>
2025-03-31 15:33:12 -0400

    initial commit

---
+++ server/src/models/Question.ts
@@ -0,0 +1,26 @@
+import { Schema, model } from 'mongoose';
+
+interface IAnswer extends Document {
+  text: string;
+  isCorrect: boolean;
+}
+
+interface IQuestion extends Document {
+  question: string;
+  answers: IAnswer[];
+}
+
+const QuestionSchema = new Schema<IQuestion>({
+  question: { type: String, required: true },
+  answers: [
+    {
+      text: { type: String, required: true },
+      isCorrect: { type: Boolean, required: true }
+    }
+  ]
+});
+
+const Question = model<IQuestion>('Question', QuestionSchema);
+export type { IQuestion };
+export { QuestionSchema };
+export default Question;
